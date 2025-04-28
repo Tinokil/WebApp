4b8a7c9a6a975276cd810b538dfe8c2bd7392a34
tinokil <125847246+Tinokil@users.noreply.github.com>
2025-04-28 17:08:23 +0300

    Delete web directory

--- web/backend/quest/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.views.decorators.http import require_GET
-
-@require_GET
-def index(request, lang='ru'):
-    return render(request, 'index.html', context={'lang': lang})
